2d19ea70d893581e1db9538e72756fba1da9b4dd
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-06-25 19:55:44 +1000

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,73 @@
-use std::{
-    error,
-    fmt::Display,
-    fs::read_to_string,
-    io::{stdin, stdout, Result, Write},
-    result,
-};
+use std::{error, fmt::Display, fs::read_to_string, io::Result, result};
 
+use pancurses::{initscr, noecho, Input, Window};
 use rand::Rng;
 
 fn main() -> result::Result<(), Box<dyn error::Error>> {
     let words = get_words()?;
     let word = pick(&words);
-    println!("the word is `{}`", word);
+    let window = create_window();
     let mut guesses_done = 1;
-    let guess = guess(&format!("{guesses_done}: "))?.trim();
+    let mut guesses = Vec::new();
+    loop {
+        window.addstr(format!("{guesses_done}: "));
+        window.refresh();
+        let mut guess = String::new();
+        loop {
+            let input = window.getch();
+            match input {
+                Some(Input::Character('\n'))
+                    if guess.len() == 5 && words.contains(&guess) && !guesses.contains(&guess) =>
+                {
+                    break;
+                }
+                Some(Input::Character('\x7f')) if window.get_cur_x() > 3 => {
+                    backspace(&window);
+                    guess.pop();
+                }
+                Some(Input::Character(character))
+                    if character.is_ascii_alphabetic() && window.get_cur_x() < 8 =>
+                {
+                    window.addch(character);
+                    guess.push(character);
+                }
+                _ => {}
+            }
+            if let Some(Input::Character(character)) = input {
+                let escaped = &character.escape_debug();
+                let (y, x) = window.get_cur_yx();
+                window.mvaddstr(
+                    window.get_max_y() - 1,
+                    window.get_max_x() - 20,
+                    "                    ",
+                );
+                window.mvaddstr(
+                    window.get_max_y() - 1,
+                    window.get_max_x() - escaped.len() as i32 - 1,
+                    escaped.to_string(),
+                );
+                window.mv(y, x);
+            }
+            window.refresh();
+        }
+        window.addch('\n');
+        window.refresh();
+        guesses_done += 1;
+        guesses.push(guess.clone());
+
+        if guess == *word {
+            window.addstr("You win!\n");
+            window.refresh();
+            break;
+        }
+    }
 
     Ok(())
+}
+
+fn backspace(window: &Window) {
+    window.mv(window.get_cur_y(), window.get_cur_x() - 1);
+    window.delch();
 }
 
 #[derive(Debug)]
@@ -47,16 +99,15 @@ fn get_words() -> Result<Vec<String>> {
         .collect())
 }
 
-fn pick<'a, T>(list: &'a Vec<T>) -> &'a T {
+fn pick<T>(list: &Vec<T>) -> &T {
     let mut rng = rand::thread_rng();
     let index = rng.gen_range(0..list.len());
     &list[index]
 }
 
-fn guess(prompt: &String) -> Result<String> {
-    print!("{}", prompt);
-    stdout().flush()?;
-    let mut guess = String::new();
-    stdin().read_line(&mut guess)?;
-    Ok(guess)
+fn create_window() -> Window {
+    let window = initscr();
+    window.keypad(true);
+    noecho();
+    window
 }
