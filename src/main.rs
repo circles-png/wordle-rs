94169d040cf49322f4422003a898c96432a81e2c
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-06-25 20:23:49 +1000

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use std::{error, fmt::Display, fs::read_to_string, io::Result, result};
+use std::{
+    error,
+    fmt::Display,
+    fs::read_to_string,
+    io::Result,
+    result,
+    time::{SystemTime, SystemTimeError},
+};
 
 use pancurses::{initscr, noecho, Input, Window};
 use rand::Rng;
 
+const MAX_DEBUG_LENGTH: i32 = 20;
+const WORD_LENGTH: i32 = 5;
+
 fn main() -> result::Result<(), Box<dyn error::Error>> {
     let words = get_words()?;
-    let word = pick(&words);
+    let (word, index) = pick(&words);
     let window = create_window();
-    let mut guesses_done = 1;
+    let mut guesses_taken = 0;
     let mut guesses = Vec::new();
+    let start = SystemTime::now();
     loop {
-        window.addstr(format!("{guesses_done}: "));
+        let guess_prompt = format!("guess {}: ", guesses_taken + 1);
+        window.addstr(guess_prompt.clone());
         window.refresh();
         let mut guess = String::new();
         loop {
@@ -21,12 +33,15 @@ fn main() -> result::Result<(), Box<dyn error::Error>> {
                 {
                     break;
                 }
-                Some(Input::Character('\x7f')) if window.get_cur_x() > 3 => {
+                Some(Input::Character('\x7f'))
+                    if window.get_cur_x() > guess_prompt.len() as i32 =>
+                {
                     backspace(&window);
                     guess.pop();
                 }
                 Some(Input::Character(character))
-                    if character.is_ascii_alphabetic() && window.get_cur_x() < 8 =>
+                    if character.is_ascii_alphabetic()
+                        && window.get_cur_x() < (guess_prompt.len() as i32 + WORD_LENGTH) =>
                 {
                     window.addch(character);
                     guess.push(character);
@@ -34,34 +49,58 @@ fn main() -> result::Result<(), Box<dyn error::Error>> {
                 _ => {}
             }
             if let Some(Input::Character(character)) = input {
-                let escaped = &character.escape_debug();
-                let (y, x) = window.get_cur_yx();
-                window.mvaddstr(
-                    window.get_max_y() - 1,
-                    window.get_max_x() - 20,
-                    "                    ",
-                );
-                window.mvaddstr(
-                    window.get_max_y() - 1,
-                    window.get_max_x() - escaped.len() as i32 - 1,
-                    escaped.to_string(),
-                );
-                window.mv(y, x);
+                display_debug(character, &window);
             }
             window.refresh();
         }
         window.addch('\n');
         window.refresh();
-        guesses_done += 1;
+        guesses_taken += 1;
         guesses.push(guess.clone());
 
         if guess == *word {
-            window.addstr("You win!\n");
-            window.refresh();
+            display_win(&window, guesses_taken, start, word, index)?;
             break;
         }
     }
 
+    Ok(())
+}
+
+fn display_debug(character: char, window: &Window) {
+    let escaped = &character.escape_debug();
+    let (y, x) = window.get_cur_yx();
+    window.mvaddstr(
+        window.get_max_y() - 1,
+        window.get_max_x() - MAX_DEBUG_LENGTH,
+        "                    ",
+    );
+    window.mvaddstr(
+        window.get_max_y() - 1,
+        window.get_max_x() - escaped.len() as i32 - 1,
+        escaped.to_string(),
+    );
+    window.mv(y, x);
+}
+
+fn display_win(
+    window: &Window,
+    guesses_taken: i32,
+    start: SystemTime,
+    word: &String,
+    index: usize,
+) -> result::Result<(), SystemTimeError> {
+    window.clear();
+    window.addstr(format!(
+        "You took {guesses_taken} guess{}
+to guess the word `{word}` (zero-based index of {index})
+in ~{:.2} seconds!
+
+",
+        if guesses_taken == 1 { "" } else { "es" },
+        start.elapsed()?.as_secs_f64()
+    ));
+    window.refresh();
     Ok(())
 }
 
@@ -70,27 +109,12 @@ fn backspace(window: &Window) {
     window.delch();
 }
 
-#[derive(Debug)]
-struct Error(String);
-
-impl Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl error::Error for Error {
-    fn description(&self) -> &str {
-        &self.0
-    }
-}
-
 fn get_words() -> Result<Vec<String>> {
     Ok(read_to_string("words")?
         .split_ascii_whitespace()
         .filter_map(|word| {
             let word = word.to_lowercase().trim().to_string();
-            if word.len() == 5 {
+            if word.len() == WORD_LENGTH as usize {
                 Some(word)
             } else {
                 None
@@ -99,10 +123,10 @@ fn get_words() -> Result<Vec<String>> {
         .collect())
 }
 
-fn pick<T>(list: &Vec<T>) -> &T {
+fn pick<T>(list: &Vec<T>) -> (&T, usize) {
     let mut rng = rand::thread_rng();
     let index = rng.gen_range(0..list.len());
-    &list[index]
+    (&list[index], index)
 }
 
 fn create_window() -> Window {
